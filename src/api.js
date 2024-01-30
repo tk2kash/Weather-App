2fbdd7de18faa9c9d313957fa73a15632bb6e804
tk2kash <79985636+tk2kash@users.noreply.github.com>
2024-01-30 19:25:08 +0000

    Add files via upload
    Weather App v1

---
+++ src/api.js
@@ -0,0 +1,12 @@
+export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
+
+export const Api_options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'd917547208mshb6f77ce43d1c672p1556abjsn38a8943bc79c',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const WEATHER_API_URL = 'https://api.openweathermap.org/data/2.5';
+export const WEATHER_API_KEY = '82809302412f9e1aaf011917aac79929';
